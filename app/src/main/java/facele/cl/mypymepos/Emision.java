152963fe5d43c193ac8146574ee7527b9b3e7562
clod <hidalgo.cl@hotmail.com>
2019-08-02 17:51:19 -0400

    #commit local

---
+++ app/src/main/java/facele/cl/mypymepos/Emision.java
@@ -0,0 +1,13 @@
+package facele.cl.mypymepos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Emision extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emision);
+    }
+}
